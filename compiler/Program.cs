d2bcef5a8b9d9dc99e1dd3e6186da4eefb079be7
AndyThePie <25379179+AndyThePie@users.noreply.github.com>
2020-02-21 09:38:26 -0500

    oh god what have i done

--- compiler/Program.cs
+++ compiler/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using Markdig;
 
 namespace compiler
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string thing = System.IO.File.ReadAllText("../blog/md/02.20.2020 - a first post.md");
+            // Finally writing the contents to a file...
+            System.IO.File.WriteAllText("../blog/thing.html",Markdig.Markdown.ToHtml(thing));
+            Console.WriteLine("hey, it maybe worked!");
+            
         }
     }
 }
